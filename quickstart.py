aac572247cd1c190ce1eb5eab091d1169f9775c2
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-18 23:13:09 +1200

    Added ship helpers for docking, orbiting, extracting

--- quickstart.py
+++ quickstart.py
@@ -60,20 +60,73 @@ def accept_contract(contract_id: str) -> dict:
     return response.json()
 
 
+def purchase_ship(ship_type: str, waypoint: str) -> dict:
+    response = requests.post(
+        f"{_url}/v2/my/ships",
+        headers={"Authorization": f"Bearer {_token}"},
+        json={
+            "shipType": ship_type,
+            "waypointSymbol": waypoint,
+        },
+    )
+    return response.json()
+
+
+def get_ship_info(ship_id: str) -> dict:
+    response = requests.get(
+        f"{_url}/v2/my/ships/{ship_id}",
+        headers={"Authorization": f"Bearer {_token}"},
+    )
+    return response.json()
+
+
+def ship_dock(ship_id: str) -> dict:
+    response = requests.post(
+        f"{_url}/v2/my/ships/{ship_id}/dock",
+        headers={"Authorization": f"Bearer {_token}"},
+    )
+    return response.json()
+
+
+def ship_orbit(ship_id: str) -> dict:
+    response = requests.post(
+        f"{_url}/v2/my/ships/{ship_id}/orbit",
+        headers={"Authorization": f"Bearer {_token}"},
+    )
+    return response.json()
+
+
+def ship_extract(ship_id: str) -> dict:
+    response = requests.post(
+        f"{_url}/v2/my/ships/{ship_id}/extract",
+        headers={"Authorization": f"Bearer {_token}"},
+    )
+    return response.json()
+
+
 def main():
-    agent_info = get_agent_info()
-    headquarters = agent_info["data"]["headquarters"]
+    output = purchase_ship("SHIP_MINING_DRONE", "X1-ZT91-25027X")
+    pprint(output)
+    # agent_info = get_agent_info()
+    # headquarters = agent_info["data"]["headquarters"]
 
-    sector, system = get_waypoint_components(headquarters)
-    waypoints = get_waypoints(system)
-    pprint(waypoints)
+    # sector, system = get_waypoint_components(headquarters)
+    # waypoints = get_waypoints(system)
+    # pprint(waypoints)
+    # waypoints_with_shipyards = [
+    #     w
+    #     for w in waypoints["data"]
+    #     if any([trait["symbol"] == "SHIPYARD" for trait in w["traits"]])
+    # ]
+    # print(waypoints_with_shipyards)
 
-    contracts = get_contracts()
-    contract = contracts["data"][0]
-    pprint(contract)
+    # contracts = get_contracts()
+    # contract = contracts["data"][0]
+    # pprint(contract)
 
 
 if __name__ == "__main__":
     import logging
+
     logging.basicConfig(level=logging.INFO)
     main()
